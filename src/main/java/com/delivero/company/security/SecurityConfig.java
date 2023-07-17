e86bc2e2f1a8dd544d218ed5fa56a5b2c563e8c9
giuseppe0908 <placidagiuseppe@gmail.com>
2023-07-17 17:34:45 +0200

    non fare nulla

--- src/main/java/com/delivero/company/security/SecurityConfig.java
+++ src/main/java/com/delivero/company/security/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.delivero.company.security;
 
 import java.util.Collections;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -11,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.delivero.company.model.User;
 import com.delivero.company.repository.UserRepository;
@@ -22,26 +25,53 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserRepository userRepository;
     
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(email -> {
-            User user = userRepository.findByEmail(email);
-            if (user == null) {
-                throw new UsernameNotFoundException(email);
-            }
-            return new org.springframework.security.core.userdetails.User(
-                user.getEmail(), user.getPassword(), Collections.emptyList());
-        });
-    }
+    // @Override
+    // protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    //     auth.userDetailsService(email -> {
+    //         User user = userRepository.findByEmail(email);
+    //         if (user == null) {
+    //             throw new UsernameNotFoundException(email);
+    //         }
+    //         return new org.springframework.security.core.userdetails.User(
+    //             user.getEmail(), user.getPassword(), Collections.emptyList());
+    //     });
+    // }
     
-     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-            .authorizeRequests()
-            .antMatchers("/api/login").permitAll()
-            .anyRequest().authenticated()
-            .and()
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-    }
+    //  @Override
+    // protected void configure(HttpSecurity http) throws Exception {
+    //     http.csrf().disable()
+    //         .authorizeRequests()
+    //         .antMatchers("/api/login").permitAll()
+    //         .anyRequest().authenticated()
+    //         .and()
+    //         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    // }
+
+    @Autowired
+	private DataSource dataSource;
+
+	@Autowired
+	public void configAuthentication(AuthenticationManagerBuilder authBuilder) throws Exception {
+		authBuilder.jdbcAuthentication()
+			.dataSource(dataSource)
+			.passwordEncoder(new BCryptPasswordEncoder())
+			.usersByUsernameQuery("select username, password, enabled from users where username=?")
+			.authoritiesByUsernameQuery("select username, role from users where username=?")
+			;
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/consoleadmin").hasRole("ADMIN")
+			.anyRequest().authenticated()
+			.and()
+			.formLogin().permitAll()
+			.and()
+			.logout().permitAll()
+			.and()
+			.exceptionHandling().accessDeniedPage("/403")
+			;
+	}
     
 }
